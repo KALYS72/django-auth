e9caea47558f14a2159f7005871ae93eea19a9c2
Kalys <jsvsjsvshsksb@gmail.com>
2022-12-06 12:21:12 +0600

    jwt auth

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from account.views import RegisterUserView, DeleteUserView
+from account.views import RegisterUserView, DeleteUserView, check_auth
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 '''-------------------------------------------Swagger docs-------------------------------------------'''
 from drf_yasg import openapi
@@ -37,5 +42,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/', swagger_view.with_ui('swagger', cache_timeout=0)),
     path('account/register/', RegisterUserView.as_view()),
-    path('account/delete/<str:email>', DeleteUserView.as_view()),
+    path('account/delete/<str:email>/', DeleteUserView.as_view()),
+    path('account/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('account/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('account/check_auth/', check_auth),
 ]
